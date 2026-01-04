001a1f771623d925a5d7802fa3399bfaf444c248
ali al zohbi <ali@ali07x.ali>
2026-01-04 18:12:27 +0200

    hi5

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "src/public/images")));
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST || "localhost",
