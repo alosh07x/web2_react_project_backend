6fb112005abef46c6ba7f640865d8a809c74f81f
ali al zohbi <ali@ali07x.ali>
2026-01-04 16:50:57 +0200

    hi4

--- src/server.js
+++ src/server.js
@@ -15,7 +15,11 @@ const db = mysql.createConnection({
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "alosh07x",
     database: process.env.DB_NAME || "csci426_project",
-    port: process.env.DB_PORT || 3306
+    port: process.env.DB_PORT || 3306,
+
+    ssl: {
+      rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
